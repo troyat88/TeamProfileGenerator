51f69d3e417ee2ffafe03ad01b4d3daa29cc81f3
Troy <troyat88@gmail.com>
2021-08-22 14:20:29 -0700

    created classes

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const Employee = require("./employee");
+const Engineer = require("./engineer");
+const Intern = require("./intern");
+const Manager = require("./manager");
+
+
